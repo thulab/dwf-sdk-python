2c92c1fe11e58085ea990cb03defbccb22dbb9c9
Lanxiaozhi <lanxiaozhidoge@gmail.com>
2019-02-27 16:00:00 +0800

    update

--- dwf/util/update.py
+++ dwf/util/update.py
@@ -5,10 +5,16 @@
 # Initial Date: 2019.02.27
 # Version 0.1
 #
+from dwf.common.log import logger
+from dwf.common.exception import *
 
 
-def auto_update(instance, args_dict):
-    for attribute, value in args_dict:
+def auto_update(instance, args_dict, check_list):
+    for attribute, value in args_dict.items():
+        if attribute in check_list:
+            if value is None:
+                logger.error('{} is needed'.format(attribute))
+                raise PARAM_LACK
         if value is not None:
             setattr(instance, attribute, value)
     return True
