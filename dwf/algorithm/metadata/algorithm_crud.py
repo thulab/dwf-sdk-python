8504ab79e392aa85a610f9549242871fdc208647
Jin-Ying <290895805@qq.com>
2019-03-16 23:19:45 +0800

    update

--- dwf/algorithm/metadata/algorithm_crud.py
+++ dwf/algorithm/metadata/algorithm_crud.py
@@ -19,9 +19,9 @@ class AlgorithmCRUD:
     def __init__(self, db_session):
         self.db_session = db_session
 
-    def add_algorithm(self, name, display_name, description, entry_name, algorithm_type, hyperparameter_config,
-                      train_input_pattern, train_output_pattern,
-                      model_input_pattern, model_output_pattern, runtime, learning=1, package_id=None):
+    def add_algorithm(self, name, display_name=None, description=None, entry_name=None, algorithm_type=None,
+                      hyperparameter_config=None, available=None, train_input_pattern=None, train_output_pattern=None,
+                      model_input_pattern=None, model_output_pattern=None, runtime=None, learning=None, package_id=None):
         #    NO.A301
         #	     Add an algorithm into the metadata DB.
         #    Args:
@@ -29,7 +29,9 @@ class AlgorithmCRUD:
         #        display_name - display name of algorithm
         #        discription - discription of algorithm
         #        entry_name - name of entry in the package
+        #        algorithm_type - type of algorithm
         #        hyperparameter_config - hyperparameter config
+        #        available - whether it is available
         #        train_input_pattern - [train_input_pattern_id1, train_input_pattern_id2 ...]
         #        train_output_pattern - [train_output_pattern_id1, train_output_pattern_id2 ...]
         #        model_input_pattern - [model_input_pattern_id1, model_input_pattern_id2 ...]
@@ -44,31 +46,17 @@ class AlgorithmCRUD:
         id = generate_primary_key('ALGO')
         create_time = datetime.now()
 
-        algorithm = Algorithm(id=id,
-                              subid=id,
-                              creator='admin',
-                              owner='admin',
-                              last_modifier='admin',
-                              create_time=create_time,
-                              name=name,
-                              display_name=display_name,
-                              algorithm_type=algorithm_type,
-                              description=description,
-                              alg_input_patterns=train_input_pattern,
-                              alg_output_patterns=train_output_pattern,
-                              parameters=hyperparameter_config,
-                              entry_name=entry_name,
-                              available=1,
-                              isbuiltin=0,
-                              isdeleted=0,
-                              islearning=learning,
-                              model_input_patterns=model_input_pattern,
-                              model_output_patterns=model_output_pattern,
-                              package_id=package_id,
-                              prog_language='python',
-                              reference_count=0,
-                              runtime=runtime
-                              )
+        if available is None:
+            available = 1
+
+        algorithm = Algorithm(id=id, subid=id, creator='admin', owner='admin', last_modifier='admin',
+                              create_time=create_time, name=name, display_name=display_name,
+                              algorithm_type=algorithm_type, description=description,
+                              alg_input_patterns=train_input_pattern, alg_output_patterns=train_output_pattern,
+                              parameters=hyperparameter_config, entry_name=entry_name, available=available,
+                              isbuiltin=0, isdeleted=0, islearning=learning,model_input_patterns=model_input_pattern,
+                              model_output_patterns=model_output_pattern, package_id=package_id, prog_language='python',
+                              reference_count=0,runtime=runtime)
         self.db_session.add(algorithm)
         self.db_session.commit()
         return id
@@ -79,17 +67,22 @@ class AlgorithmCRUD:
                          package_id=None):
         #    NO.A302
         #        Update a deep-learning algorithm into the metadata DB.
-        #        Attention: It is DANGEROUS that the last last hyperparameter_config will be covered.
+        #        Attention: It is DANGEROUS that the last hyperparameter_config will be covered.
         #    Args:
+        #        algorithm_id - id of algorithm
         #        name - name of algorithm
-        #        main_file_name - name of main file in the package
+        #        display_name - display name of algorithm
+        #        discription - discription of algorithm
+        #        entry_name - name of entry in the package
+        #        algorithm_type - type of algorithm
         #        hyperparameter_config - hyperparameter config
+        #        available - whether it is available
         #        train_input_pattern - [train_input_pattern_id1, train_input_pattern_id2 ...]
         #        train_output_pattern - [train_output_pattern_id1, train_output_pattern_id2 ...]
         #        model_input_pattern - [model_input_pattern_id1, model_input_pattern_id2 ...]
         #        model_output_pattern - [model_output_pattern_id1, model_output_pattern_id2 ...]
-        #        example_model_id - id of example model
-        #        learning - whether it is a learning algorithm
+        #        runtime - running environment eg. xlearn.pytorch/ xlearn.tensorflow
+        #        learning - whether it is a learning algorithm, 0/1
         #        package_id - id of package
         #    Returns:
         #        Algorithm id.
@@ -141,9 +134,9 @@ class AlgorithmCRUD:
         self.db_session.commit()
         return algorithm_id
 
-    def query_algorithm(self, algorithm_id):
+    def get_algorithm(self, algorithm_id):
         #    NO.A312
-        #	     Query algorithm information.
+        #	     Get algorithm information.
         #    Args：
         #	     algorithm_id - The ID of algorithm.
         #    Returns：
@@ -156,21 +149,19 @@ class AlgorithmCRUD:
             raise NON_EXISTING_ALGORITHM
         return algorithm
 
-    def query_algorithms(self):
+    def get_all_algorithms(self):
         #    NO.A313
-        #        Add a deep-learning algorithm into the metadata DB.
+        #        Get all algorithms' information.
         #    Args：
-        #        algorithm_id - The ID of algorithm.
+        #        None
         #    Returns：
-        #        algorithm_info
-        #    Exceptions:
-        #        NON_EXISTING_ALGORITHM - The given algorithm_id does not exist.
+        #        algorithm_info list
         algorithms = self.db_session.query(Algorithm).all()
         return algorithms
 
     def delete_algorithm(self, algorithm_id):
         #    NO.A314
-        #	     Set "deleted" of algorithm to False in the metadata DB. The algorithm cannot be used after deleted,
+        #	     Set "isdeleted" of algorithm to 1 in the metadata DB. The algorithm cannot be used after deleted,
         #        It's still saved in DB. Thus when needed, deleted algorithms can be recovered.
         #        If an algorithm is running, it cannot be deleted.
         #    Args:
@@ -179,7 +170,6 @@ class AlgorithmCRUD:
         #	     None.
         #    Exceptions:
         #        NON_EXISTING_ALGORITHM - The given algorithm_id does not exist.
-        #        DELETE_FAILURE - The algorithm cannot be deleted.
         algorithm = self.db_session.query(Algorithm).get(algorithm_id)
         if algorithm is None:
             logger.error('Algorithm is not found')
@@ -196,8 +186,6 @@ class AlgorithmCRUD:
         #        None.
         #    Exceptions:
         #        NON_EXISTING_ALGORITHM - The given algorithm_id does not exist.
-        #        NOT_DELETED - The algorithm is not deleted.
-        #        RECOVER_FAILURE - The algorithm cannot be recovered.
         algorithm = self.db_session.query(Algorithm).get(algorithm_id)
         if algorithm is None:
             logger.error('Algorithm is not found')
@@ -223,8 +211,8 @@ class AlgorithmCRUD:
 
     def clean_algorithm(self, algorithm_id):
         #    NO.A317
-        #	     Set "deleted" of algorithm to False in the metadata DB. The algorithm cannot be used after deleted,
-        #        It's still saved in DB. Thus when needed, deleted algorithms can be recovered.
+        #	     Clean one algorithm.
+        #        It will be no longer saved in DB. It cannot be recovered.
         #        If an algorithm is using, it cannot be deleted.
         #    Args:
         #	     algorithm_id - The ID of algorithm.
@@ -232,7 +220,6 @@ class AlgorithmCRUD:
         #	     None.
         #    Exceptions:
         #        NON_EXISTING_ALGORITHM - The given algorithm_id does not exist.
-        #        DELETE_FAILURE - The algorithm cannot be deleted.
         algorithm = self.db_session.query(Algorithm).get(algorithm_id)
         if algorithm is None:
             logger.error('Algorithm is not found')
