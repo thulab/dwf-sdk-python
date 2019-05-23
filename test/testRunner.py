9ca3ed1ce2b87650e742957cfedeef02dc5d9610
wendyJing <daiwenjing001@163.com>
2019-05-23 17:18:17 +0800

    commit testcase

--- test/testRunner.py
+++ test/testRunner.py
@@ -2,11 +2,11 @@ import unittest
 import time
 from test.util import HTMLTestRunner
 
-discover = unittest.defaultTestLoader.discover("./test/cases", pattern="test_*.py")
-runner = unittest.TextTestRunner()
-runner.run(discover)
-
-
 # discover = unittest.defaultTestLoader.discover("./test/cases", pattern="test_*.py")
-# with open("./test/reports/dwf-sdk-python%s.html" % time.strftime("%Y%m%d%H%M%S"),"wb") as f:
-#     HTMLTestRunner.HTMLTestRunner(f,title="dwf-sdk-python",description="win10").run(discover)
+# runner = unittest.TextTestRunner()
+# runner.run(discover)
+
+
+discover = unittest.defaultTestLoader.discover("./test/cases", pattern="test_*.py")
+with open("./test/reports/dwf-sdk-python%s.html" % time.strftime("%Y%m%d%H%M%S"),"wb") as f:
+    HTMLTestRunner.HTMLTestRunner(f,title="dwf-sdk-python",description="win10").run(discover)
