e9de62790b087194fb1d291dcf86809b68f962a1
Lanxiaozhi <lanxiaozhidoge@gmail.com>
2019-06-23 16:37:06 +0800

    update

--- dwf/datasource/metadata/datasource_crud.py
+++ dwf/datasource/metadata/datasource_crud.py
@@ -1,9 +1,21 @@
 # -*- coding:utf-8 -*-
+#
+# DataWay Dataset SDK
+# Initial Date: 2018.06.14
+#
+# Title: methods for metadata of datasource
+#
+# Version 0.1
+#
+
+from dwf.common.exception import *
+from dwf.common.log import logger
 from dwf.ormmodels import Datasource, datetime
 from dwf.util.id import generate_primary_key
 
 
 class DataSourceCRUD:
+
     def __init__(self, db_session):
         self.db_session = db_session
 
@@ -11,83 +23,10 @@ class DataSourceCRUD:
                        data_file_format=None, database_name=None, datasource_type='LOCAL_FS', description=None,
                        folder_depth=None, paramone=None, password=None, server_ip=None, server_port=None, username=None,
                        workbench_url=None):
-        '''
-            Register a datasource in the metadata DB of DWF.
-            Args:
-                id -
-                create_time -
-                update_time -
-                name - Name of DB.
-                hostname - The hostname or IP of DB.
-                port - The Port of DB.
-                username - The username used for login.
-                password - The password of DB.
-                datasource_type -  The type of datasource, like HDFS.
-                view_metadata_port -
-                description - The description of datasource
-            Returns:
-                The ID of datasource.
-
-        '''
-        id = generate_primary_key('DSOU')
-        create_time = datetime.now()
-
-        if database_name is None:
-            database_name = '/'
-        if folder_depth is None:
-            folder_depth = -1
-
-        new_datasource = Datasource(id=id, subid=subid, creator=creator, owner=owner, current_process=current_process,
-                                    last_modifier=last_modifier, create_time=create_time, name=name,
-                                    database_name=database_name, data_file_format=data_file_format,
-                                    datasource_type=datasource_type, description=description, folder_depth=folder_depth,
-                                    paramone=paramone, password=password, server_ip=server_ip, server_port=server_port,
-                                    username=username, workbench_url=workbench_url)
-        self.db_session.add(new_datasource)
-        self.db_session.commit()
-        return new_datasource.id
-
-    def get_datasource(self, datasource_id):
-        '''
-            Get a datasource by ID from the metadata DB of DWF.
-            Args:
-                datasource_id - The ID of datasource.
-            Returns:
-                The object of datasource.
-        '''
-        datasource = self.db_session.query(Datasource).get(datasource_id)
-        return datasource
-
-    def get_all_datasource(self):
-        '''
-            Get all datasources from the metadata DB of DWF.
-            Args:
-
-            Returns:
-                The list of datasources.
-        '''
-        datasource_list = self.db_session.query(Datasource).all()
-        return datasource_list
-
-    def delete_datasource(self, datasource_id):
-        '''
-            Delete a datasource by ID from the metadata DB of DWF.
-            Args:
-                datasource_id - The ID of datasource.
-        '''
-        pending = self.db_session.query(Datasource).get(datasource_id)
-        self.db_session.delete(pending)
-        self.db_session.commit()
-        return True
-
-    def update_datasource(self, datasource_id, name=None, subid=None, creator=None, owner=None, current_process=None,
-                          last_modifier=None, data_file_format=None, database_name=None, datasource_type=None,
-                          description=None, folder_depth=None, paramone=None, password=None, server_ip=None,
-                          server_port=None, username=None, workbench_url=None):
         """
+        创建数据源元信息
 
-        :param datasource_id:
-        :param name:
+        :param name: 数据源名称
         :param subid:
         :param creator:
         :param owner:
@@ -104,8 +43,101 @@ class DataSourceCRUD:
         :param server_port:
         :param username:
         :param workbench_url:
-        :return:
+        :return: 数据源ID
         """
+
+        id = generate_primary_key('DSOU')
+        create_time = datetime.now()
+
+        if creator is None:
+            creator = 'admin'
+        if owner is None:
+            owner = 'admin'
+        if last_modifier is None:
+            last_modifier = 'admin'
+        if database_name is None:
+            database_name = '/'
+        if folder_depth is None:
+            folder_depth = -1
+
+        new_datasource = Datasource(id=id, subid=subid, creator=creator, owner=owner, current_process=current_process,
+                                    last_modifier=last_modifier, create_time=create_time, name=name,
+                                    database_name=database_name, data_file_format=data_file_format,
+                                    datasource_type=datasource_type, description=description, folder_depth=folder_depth,
+                                    paramone=paramone, password=password, server_ip=server_ip, server_port=server_port,
+                                    username=username, workbench_url=workbench_url)
+        self.db_session.add(new_datasource)
+        self.db_session.commit()
+
+        return id
+
+    def get_datasource(self, datasource_id):
+        """
+        根据ID获取数据源元信息
+
+        :param datasource_id: 数据源ID
+        :return: 数据源元信息
+        """
+
+        datasource = self.db_session.query(Datasource).get(datasource_id)
+
+        return datasource
+
+    def get_all_datasource(self):
+        """
+        获取全部的数据源元信息
+
+        :return: 数据源元信息列表
+        """
+
+        datasource_list = self.db_session.query(Datasource).all()
+
+        return datasource_list
+
+    def delete_datasource(self, datasource_id):
+        """
+        删除数据源元信息
+
+        :param datasource_id: 数据源ID
+        :return: 无
+        """
+
+        pending = self.db_session.query(Datasource).get(datasource_id)
+        self.db_session.delete(pending)
+        self.db_session.commit()
+
+    def update_datasource(self, datasource_id, name=None, subid=None, creator=None, owner=None, current_process=None,
+                          last_modifier=None, data_file_format=None, database_name=None, datasource_type=None,
+                          description=None, folder_depth=None, paramone=None, password=None, server_ip=None,
+                          server_port=None, username=None, workbench_url=None):
+        """
+        更新数据源元信息
+
+        :param datasource_id: 数据源ID
+        :param name: 数据源名称
+        :param subid:
+        :param creator:
+        :param owner:
+        :param current_process:
+        :param last_modifier:
+        :param data_file_format:
+        :param database_name:
+        :param datasource_type:
+        :param description:
+        :param folder_depth:
+        :param paramone:
+        :param password:
+        :param server_ip:
+        :param server_port:
+        :param username:
+        :param workbench_url:
+        :return: 无
+        """
+
+        if datasource_id is None:
+            logger.error('缺少数据源ID')
+            raise PARAM_LACK
+
         pending = self.db_session.query(Datasource).get(datasource_id)
 
         if subid is not None:
@@ -145,4 +177,3 @@ class DataSourceCRUD:
 
         pending.update_time = datetime.now()
         self.db_session.commit()
-        return pending
