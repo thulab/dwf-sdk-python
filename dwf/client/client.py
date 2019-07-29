eb527e02a7a15de401b578841f941d334f76351d
Jin-Ying <290895805@qq.com>
2019-07-29 20:53:12 +0800

    fix

--- dwf/client/client.py
+++ dwf/client/client.py
@@ -8,42 +8,45 @@ from dwf.common.config import deploy_config
 from dwf.datapattern.metadata.image_folder4_classification import ImageFolder4Classfication
 from dwf.datapattern.metadata.two_image_folder4_detection_txt import TwoImageFolder4Detection_txt
 
-def upload_algorithm(algo_id, server_url, filename, algorithm_name, description, requirements, entrance, mirror, sample_dataset_path):
+def upload_algorithm(algorithm_name, description, requirements=None, entrance=None, mirror=None, sample_dataset_path=None):
     kilobytes = 1024
     megabytes = kilobytes * 1000
     chunk_size = int(2 * megabytes)
+    server_url = "http://192.168.35.59:30800/api/engine"
+    filename = "/Users/sherry/GitHub/Xlearn-Algorithms/SSD/SSD.zip"
 
     part_num = 0
     input_file = open(filename, 'rb')
     real_name = os.path.basename(filename)
     headers = {
-        'Authorization': "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NjM4NjI4OTUsImlhdCI6MTU2Mzg1OTI5NSwibmJmIjoxNTYzODU5Mjk1LCJ1c2VyX2lkIjoiVVNFUjQ0YzJmMDg3MTFlODhkNDkzNGUxMmRkMDdjMDciLCJ1c2VybmFtZSI6InhsZWFybiJ9.x-CgduDbYiWpiAJ_A0NCd3mjtuB5C6ZtUPwdxc-EoYM"
+        'Authorization': "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NjQ0MDUzMTIsImlhdCI6MTU2NDQwMTcxMiwibmJmIjoxNTY0NDAxNzEyLCJ1c2VyX2lkIjoiVVNFUjQ0YzJmMDg3MTFlODhkNDkzNGUxMmRkMDdjMDciLCJ1c2VybmFtZSI6InhsZWFybiJ9.ayAqhDE1FvsuF4gZ_jVNif70F3r-wsBPixiLQxvbiHU"
     }
-
-    f = zipfile.ZipFile(sample_dataset_path,'r')
-    for file in f.namelist():
-        f.extract(file,"./tmp/")
-
-    try:
-        pattern = TwoImageFolder4Detection_txt()
-        pattern.check('./tmp/')
-        patterns = pattern.generate('./tmp')
-    except:
-        pass
-
-    try:
-        pattern = ImageFolder4Classfication()
-        pattern.check('./tmp/')
-        patterns = pattern.generate('./tmp')
-    except:
-        pass
-
-    shutil.rmtree('./tmp/')
+    patterns = ''
+    # if sample_dataset_path is not None:
+    #     f = zipfile.ZipFile(sample_dataset_path,'r')
+    #     for file in f.namelist():
+    #         f.extract(file,"./tmp/")
+    #
+    #     try:
+    #         pattern = TwoImageFolder4Detection_txt()
+    #         pattern.check('./tmp/')
+    #         patterns = pattern.generate('./tmp')
+    #     except:
+    #         pass
+    #
+    #     try:
+    #         pattern = ImageFolder4Classfication()
+    #         pattern.check('./tmp/')
+    #         patterns = pattern.generate('./tmp')
+    #     except:
+    #         pass
+    #
+    #     shutil.rmtree('./tmp/')
 
     data = {
         'name': algorithm_name,
         'description': description,
-        # 'filename': real_name,
+        'filename': real_name,
         # 'requirements': requirements,
         'entry_name': entrance,
         'train_input_pattern':patterns
@@ -52,13 +55,13 @@ def upload_algorithm(algo_id, server_url, filename, algorithm_name, description,
         # 'usage': 3,
         # 'owner': deploy_config.get("CLUSTER", "OWNER"),
     }
-    response = requests.post(server_url + '/algorithm/upload', data=data, headers=headers)
-    print(response.text)
+    response = requests.post(server_url + '/algorithm/add', data=data, headers=headers)
+    print(response)
     response = response.json()
     print("=" * 50)
     print(response)
     print("=" * 50)
-    file_id = response['file_id']
+    file_id = response['data']
     while True:
         chunk = input_file.read(chunk_size)
         if not chunk:
@@ -71,59 +74,64 @@ def upload_algorithm(algo_id, server_url, filename, algorithm_name, description,
             'chunk': part_num,
         }
         print("Uploading chunk %d:" % part_num)
-        print("Response:", requests.post(server_url + '/upload', data=data, headers=headers, files=files).text)
+        print("Response:", requests.post(server_url + '/algorithm/upload', data=data, headers=headers, files=files).text)
         part_num += 1
     input_file.close()
 
     data = {
         'file_id': file_id,
     }
-    print("Response:", requests.post(server_url + '/upload_finish', data=data, headers=headers).text)
+    print("Response:", requests.post(server_url + '/algorithm/upload_finish', data=data, headers=headers).text)
 
 
 def upload_dataset(dataset_name, description):
     kilobytes = 1024
     megabytes = kilobytes * 1000
     chunk_size = int(2 * megabytes)
-    server_url = "http://192.168.10.22:30799/api/engine"
-    filename = "/Users/sherry/Desktop/xlearn_data/0716.zip"
+    server_url = "http://192.168.35.59:30800/api/engine"
+    filename = "/Users/sherry/Desktop/xlearn_data/jilv.zip"
 
     part_num = 0
     input_file = open(filename, 'rb')
     real_name = os.path.basename(filename)
     headers = {
-        'Authorization': "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NjM4NjI4OTUsImlhdCI6MTU2Mzg1OTI5NSwibmJmIjoxNTYzODU5Mjk1LCJ1c2VyX2lkIjoiVVNFUjQ0YzJmMDg3MTFlODhkNDkzNGUxMmRkMDdjMDciLCJ1c2VybmFtZSI6InhsZWFybiJ9.x-CgduDbYiWpiAJ_A0NCd3mjtuB5C6ZtUPwdxc-EoYM"
+        'Authorization': "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NjQ0MDUzMTIsImlhdCI6MTU2NDQwMTcxMiwibmJmIjoxNTY0NDAxNzEyLCJ1c2VyX2lkIjoiVVNFUjQ0YzJmMDg3MTFlODhkNDkzNGUxMmRkMDdjMDciLCJ1c2VybmFtZSI6InhsZWFybiJ9.ayAqhDE1FvsuF4gZ_jVNif70F3r-wsBPixiLQxvbiHU"
     }
+    patterns = ''
 
-    f = zipfile.ZipFile(filename,'r')
-    for file in f.namelist():
-        f.extract(file,"./tmp/")
-
-    try:
-        pattern = TwoImageFolder4Detection_txt()
-        pattern.check('./tmp/')
-        patterns = pattern.generate('./tmp')
-    except:
-        pass
-
-    try:
-        pattern = ImageFolder4Classfication()
-        pattern.check('./tmp/')
-        patterns = pattern.generate('./tmp')
-    except:
-        pass
-
-    shutil.rmtree('./tmp/')
+    # f = zipfile.ZipFile(filename,'r')
+    # for file in f.namelist():
+    #     f.extract(file,"./tmp/")
+    #
+    # try:
+    #     pattern = TwoImageFolder4Detection_txt()
+    #     pattern.check('./tmp/')
+    #     patterns = pattern.generate('./tmp')
+    # except:
+    #     pass
+    #
+    # try:
+    #     pattern = ImageFolder4Classfication()
+    #     pattern.check('./tmp/')
+    #     patterns = pattern.generate('./tmp')
+    # except:
+    #     pass
+    #
+    # shutil.rmtree('./tmp/')
 
     data = {
         'name': dataset_name,
         'datasource_id': 'DSOU30d2d78e6cb211e98f720a580af4',
         'description': description,
         'filename': real_name,
-        'patterns': patterns,
+        'patterns': patterns
         # 'data_file_format': '',
+        # 'user': 'USER44c2f08711e88d4934e12dd07c07',
+        # 'username': 'xlearn'
+
     }
     response = requests.post(server_url + '/dataset/add', data=data, headers=headers)
+    print(response)
     response = response.json()
     print("=" * 50)
     print(response)
@@ -150,4 +158,6 @@ def upload_dataset(dataset_name, description):
     }
     print("Response:", requests.post(server_url + '/dataset/upload_finish', data=data, headers=headers).text)
 
-upload_dataset('','')
+# upload_dataset('jilv','ji lv')
+# upload_algorithm(algorithm_name="SSD", \
+#                  description="SSD", entrance='ssd_train.train')
